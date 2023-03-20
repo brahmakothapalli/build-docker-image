0155b14eea3fb184c91849ab062c04f0d9555149
vkottapalli <vkottapalli@tmhcc.com>
2023-03-20 18:39:39 +0530

    first commit

---
+++ src/test/java/tests/HelloWorldImage.java
@@ -0,0 +1,8 @@
+package tests;
+
+public class HelloWorldImage {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Building Docker Images using Jenkins Pipeline Project!!");
+    }
+}
